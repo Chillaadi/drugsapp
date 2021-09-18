b1142b88c5d431bfaf16304e3f96a173f9514133
Chillaadi <88712918+Chillaadi@users.noreply.github.com>
2021-09-18 17:01:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,16 @@ def prediction():
     Na_to_K = request.form['Na_to_K']
 
     prediction= loadedModel.predict([[Age,Sex,BP,Cholesterol,Na_to_K ]])[0]
-    
+    If prediction == drugX:
+        prediction = " Use Sodium polystyrene sulfonate (Kayexalate)"
+    Elif prediction == drugY:
+        prediction = " Use Sodium polystyrene sulfonate (e.g.Kayexalate) "
+    Elif prediction == drugC:
+        prediction = " use miscellaneous cardiovascular agents midodrine "
+    Elif prediction == drugA:
+        prediction = "ACE inhibitor or an angiotensin-2 receptor blocker (ARB)"
+    Else : 
+        prediction = "calcium channel blocker"
     return render_template('index.html', api_output=prediction)
    
 #main Function
