c54c6cb72c5433da000e63a0328b16fcaae1cc9e
Chillaadi <88712918+Chillaadi@users.noreply.github.com>
2021-09-18 11:22:37 +0530

    Add files via upload

---
+++ drugsapp.py
@@ -0,0 +1,22 @@
+import pickle 
+from flask import Flask , render_template, request
+ #global variables 
+loadedModel = pickle.load(open("DRUGS CLASSIFICATION.pkl",'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/prediction', methods = ['POST'])
+def prediction():
+    Age = request.form['Age']
+    Sex = request.form['Sex']
+    BP =  request.form['BP']
+    Cholesterol = request.form['Cholesterol']
+    Na_to_K = request.form['Na_to_K']
+
+    prediction= loadedModel.predict([[Age,Sex,BP,Cholesterol,Na_to_K ]])[0]
+    
+    return render_template('index.html', api_output=prediction)
+
+app.run(debug=True)
