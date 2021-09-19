bc4f19824ac69709ba8ccf9ed723d2c17bb96ba6
Chillaadi <88712918+Chillaadi@users.noreply.github.com>
2021-09-19 20:19:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,18 +16,7 @@ def prediction():
     Na_to_K = request.form['Na_to_K']
 
     prediction= loadedModel.predict([[Age,Sex,BP,Cholesterol,Na_to_K ]])[0]
-    if prediction=drugA:
-        prediction =" Use ACE inhibitor or an angiotensin-2 receptor blocker (ARB)"
-    elif prediction =drugB:
-        prediction =" Use calcium channel blocker"
-    elif prediction= drugC:
-        prediction = "Use Midodrine"
-    elif prediction=drugX:
-        prediction = "Eat more natural foods"
-    else:
-        prediction =" Eat more natural foods"
-        
-    
+   
     return render_template('index.html', api_output=prediction)
    
 #main Function
